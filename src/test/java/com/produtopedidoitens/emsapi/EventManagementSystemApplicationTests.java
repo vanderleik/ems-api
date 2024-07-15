8205016010061b14df239859eb786f82324f2de9
vanderleik <vanderleik@yahoo.com.br>
2024-07-15 13:56:08 -0300

    feat: Configurando o projeto

--- src/test/java/com/produtopedidoitens/emsapi/EventManagementSystemApplicationTests.java
+++ src/test/java/com/produtopedidoitens/emsapi/EventManagementSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
