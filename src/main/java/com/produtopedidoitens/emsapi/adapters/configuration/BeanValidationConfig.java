fa8fae430a7ecde29706c87d03623b820547a189
vanderleik <vanderleik@yahoo.com.br>
2024-07-15 16:11:25 -0300

    feat: configurando Bean Validation

---
+++ src/main/java/com/produtopedidoitens/emsapi/adapters/configuration/BeanValidationConfig.java
@@ -0,0 +1,28 @@
+package com.produtopedidoitens.emsapi.adapters.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class BeanValidationConfig {
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(MessageSource messageSource) {
+        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        factoryBean.setValidationMessageSource(messageSource);
+        return factoryBean;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+
+}
