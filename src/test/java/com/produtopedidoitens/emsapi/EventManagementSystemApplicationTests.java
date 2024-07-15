9e89497b9e3546b1afd9c255b0817f15e6a9996f
vanderleik <vanderleik@yahoo.com.br>
2024-07-15 13:54:28 -0300

    first commit

---
+++ src/test/java/com/produtopedidoitens/emsapi/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtopedidoitens.emsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
