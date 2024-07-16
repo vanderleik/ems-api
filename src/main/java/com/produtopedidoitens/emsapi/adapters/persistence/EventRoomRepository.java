2a712c82307ac93e13eaea05c6c1d7356815302f
vanderleik <vanderleik@yahoo.com.br>
2024-07-16 14:19:12 -0300

    feat: testes unitários com LockMode Otimista

--- src/main/java/com/produtopedidoitens/emsapi/adapters/persistence/EventRoomRepository.java
+++ src/main/java/com/produtopedidoitens/emsapi/adapters/persistence/EventRoomRepository.java
@@ -1,11 +1,18 @@
 package com.produtopedidoitens.emsapi.adapters.persistence;
 
 import com.produtopedidoitens.emsapi.application.domain.entities.EventRoomEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.UUID;
 
 public interface EventRoomRepository extends JpaRepository<EventRoomEntity, UUID>, QuerydslPredicateExecutor<EventRoomEntity> {
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("SELECT e FROM EventRoomEntity e WHERE e.roomName = :roomName")
+    EventRoomEntity findByRoomName(String roomName);
 
 }
